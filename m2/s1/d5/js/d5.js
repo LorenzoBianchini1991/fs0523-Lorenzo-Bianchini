79adce1a9f2f906a3f76998a67d982ae845cecfa
LorenzoBianchni1991 <bianchnilorenzo91@gmail.com>
2023-10-28 03:46:50 +0200

    m2s1d5

--- m2/s1/d5/js/d5.js
+++ m2/s1/d5/js/d5.js
@@ -15,7 +15,7 @@ window.addEventListener('scroll', function() {
     const getStartedLink = document.querySelector('.button'); 
     const scrollPosition = window.scrollY;
 
-    if (scrollPosition > 475) {
+    if (scrollPosition > 275) {
         header.style.backgroundColor = 'white'; 
         getStartedLink.style.backgroundColor = 'var(--verde)'; 
     } else {
@@ -23,3 +23,60 @@ window.addEventListener('scroll', function() {
         getStartedLink.style.backgroundColor = 'var(--nero1)'; 
     }
 });
+
+/* TOOL-BOX*/
+
+const authorLink = document.getElementById('author-link');
+const tooltip = document.getElementById('tooltip');
+
+let tooltipTimer;
+
+function showTooltip() {
+    clearTimeout(tooltipTimer);
+    tooltip.style.display = 'block';
+}
+
+function hideTooltip() {
+    tooltipTimer = setTimeout(() => {
+        tooltip.style.display = 'none';
+    }, 500);
+}
+
+authorLink.addEventListener('mouseover', showTooltip);
+authorLink.addEventListener('mouseout', hideTooltip);
+tooltip.addEventListener('mouseover', showTooltip);
+tooltip.addEventListener('mouseout', hideTooltip);
+
+/* STAR */
+
+const stella = document.getElementById('stella');
+const boxStellaGrigio = document.getElementById('box-stella-grigio');
+let boxStellaGrigioVisible = false;
+
+function showBoxStellaGrigio() {
+    boxStellaGrigio.style.display = 'block';
+    boxStellaGrigioVisible = true;
+}
+
+function hideBoxStellaGrigio() {
+    boxStellaGrigioVisible = false;
+    setTimeout(() => {
+        if (!boxStellaGrigioVisible) {
+            boxStellaGrigio.style.display = 'none';
+        }
+    }, 500);
+}
+
+stella.addEventListener('mouseover', showBoxStellaGrigio);
+stella.addEventListener('mouseout', hideBoxStellaGrigio);
+
+boxStellaGrigio.addEventListener('mouseover', showBoxStellaGrigio);
+boxStellaGrigio.addEventListener('mouseout', hideBoxStellaGrigio);
+
+/* stella tooltip */
+
+
+
+
+/* MAIN INFINTE SCROLL */
+
