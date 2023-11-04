f4e376ecafa4086efb41fd04e1c6b8793736e382
LorenzoBianchni1991 <bianchnilorenzo91@gmail.com>
2023-11-04 22:53:04 +0100

    m2s2d5

--- m2/s2/d5/assets/js/landing.js
+++ m2/s2/d5/assets/js/landing.js
@@ -21,9 +21,21 @@ function position(id){
   console.log(id)
 }
 
-// DROPDOWN TOGGLE GENRES
+// DROPDOWN GENRES
 
-var dropdownElementList = [].slice.call(document.querySelectorAll('.dropdown-toggle-genres'))
-var dropdownList = dropdownElementList.map(function (dropdownToggleEl) {
-  return new bootstrap.Dropdown(dropdownToggleEl)
-})
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
