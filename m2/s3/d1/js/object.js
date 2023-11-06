99a631c9c7c38c0f870dcb1adaf599251d742b3c
LorenzoBianchni1991 <bianchnilorenzo91@gmail.com>
2023-11-06 17:41:03 +0100

    m2s3d1

--- m2/s3/d1/js/object.js
+++ m2/s3/d1/js/object.js
@@ -40,6 +40,50 @@ function confrontAllUsers(users) {
 confrontAllUsers(users);
 
 
+//CONSTRUCTOR
+
+
+class User {
+    constructor(firstName, lastName, age, location) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.location = location;
+    }
+
+    confrontAge(otherUser) {
+        if (this.age > otherUser.age) {
+            return `${this.firstName} è più vecchio di ${otherUser.firstName}`;
+        } else if (this.age < otherUser.age) {
+            return `${this.firstName} è più giovane di ${otherUser.firstName}`;
+        } else {
+            return `${this.firstName} ha la stessa età di ${otherUser.firstName}`;
+        }
+    }
+}
+
+const users = [
+    new User("Mario", "Rossi", 30, "Roma"),
+    new User("Luigi", "Bianchi", 25, "Milano"),
+    new User("Antonio", "Diani", 47, "Taranto"),
+    new User("Andrea", "Pieri", 18, "Bologna"),
+    new User("Carlo", "Vinci", 61, "Genova")
+];
+
+function confrontAllUsers(users) {
+    for (let i = 0; i < users.length; i++) {
+        for (let j = 0; j < users.length; j++) {
+            if (i !== j) {
+                const comparisonResult = users[i].confrontAge(users[j]);
+                console.log(comparisonResult);
+            }
+        }
+    }
+}
+
+confrontAllUsers(users);
+
+
 //ESERCIZIO 2
 
 document.getElementById('addPetButton').addEventListener('click', createPet);
