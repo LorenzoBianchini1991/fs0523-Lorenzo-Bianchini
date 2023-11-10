205b318fa59f54f6332d0fff647aa6345e9e1725
LorenzoBianchni1991 <bianchnilorenzo91@gmail.com>
2023-11-10 15:50:54 +0100

    m2s3d5

--- m2/s3/d5/assets/js/homepage.js
+++ m2/s3/d5/assets/js/homepage.js
@@ -1,8 +1,28 @@
-// Contenuto specifico per la homepage
-function getProducts() {
-    // ... codice specifico per ottenere la lista dei prodotti ...
-  }
-  
-  function showProductDetails(productId) {
-    // ... codice specifico per visualizzare i dettagli del prodotto ...
-  }
+document.addEventListener('DOMContentLoaded', function () {
+  const mockProducts = [
+    { _id: '1', name: 'Product 1' },
+    { _id: '2', name: 'Product 2' },
+  ];
+
+  const productList = document.getElementById('productList');
+  const productTemplate = document.getElementById('productTemplate');
+
+  mockProducts.forEach(product => {
+    const listItem = productTemplate.content.cloneNode(true);
+    const productNameSpan = listItem.querySelector('.product-name');
+    const detailsButton = listItem.querySelector('.details-button');
+
+    productNameSpan.textContent = product.name;
+    detailsButton.dataset.productId = product._id;
+
+    detailsButton.addEventListener('click', function () {
+      showProductDetails(product._id);
+    });
+
+    productList.appendChild(listItem);
+  });
+});
+
+function showProductDetails(productId) {
+  window.location.href = `productDetail.html?productId=${productId}`;
+}
--- m2/s3/d5/assets/js/productdetail.js
+++ m2/s3/d5/assets/js/productdetail.js
@@ -1,4 +1,37 @@
-// Contenuto specifico per la pagina di dettaglio del prodotto
-function showProductDetails(productId) {
-    // ... codice specifico per visualizzare i dettagli del prodotto ...
+document.addEventListener('DOMContentLoaded', function () {
+  const urlParams = new URLSearchParams(window.location.search);
+  const productId = urlParams.get('productId');
+
+  if (productId) {
+    getProductDetails(productId);
+  } else {
+    console.error('Product ID not found in URL');
   }
+});
+
+async function getProductDetails(productId) {
+  try {
+    const response = await fetch(`${apiUrl}/${productId}`, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    });
+
+    if (response.ok) {
+      const product = await response.json();
+      const productDetails = document.getElementById('productDetails');
+
+      productDetails.innerHTML = '';
+
+      for (const key in product) {
+        const detailItem = document.createElement('div');
+        detailItem.innerHTML = `<strong>${key}:</strong> ${product[key]}`;
+        productDetails.appendChild(detailItem);
+      }
+    } else {
+      console.error('Error fetching product details:', response.statusText);
+    }
+  } catch (error) {
+    console.error('Error fetching product details:', error);
+  }
+}
